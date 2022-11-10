6829849fa01a617cb538fedb7d5ebc3f30c75ea6
DorGeyerGit <117093805+DorGeyerGit@users.noreply.github.com>
2022-11-10 17:19:00 +0200

    Update Assignment02Q02.java

--- src/Assignment02Q02.java
+++ src/Assignment02Q02.java
@@ -5,11 +5,13 @@ public class Assignment02Q02 {
 	public static void main(String[] args) {
 		// do not change this part below
 		double piEstimation = 0.0;
-		
-		// *** your code goes here below ***
-		
-		
-		// do not change this part below
+		int len = Integer.parseInt(args[0]);
+		for (double i=0 ; i<len; i++) {
+			double y = Math.pow(-1, i)*(1/(2*(i)+1));
+			piEstimation=piEstimation+y;
+			
+		}
+		piEstimation=4*piEstimation;
 		System.out.println(piEstimation + " " + Math.PI);
 
 	}
