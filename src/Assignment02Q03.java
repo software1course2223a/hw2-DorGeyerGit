5a8e1c0e8e8e4f2d9630ae1393726aa6ccd2d3aa
DorGeyerGit <117093805+DorGeyerGit@users.noreply.github.com>
2022-11-10 17:57:19 +0200

    Update Assignment02Q03.java

--- src/Assignment02Q03.java
+++ src/Assignment02Q03.java
@@ -2,13 +2,24 @@
 public class Assignment02Q03 {
 
 	public static void main(String[] args) {
-		int numOfOdd = 0;
-		int n = -1;
-		// *** your code goes here below ***
+		int numOfOdd = 2;
+		String series = "1 1 2";
+		int last = 2;
+		int before_last= 1;
+		int len = Integer.parseInt(args[0]);
+		for (int i=0; i<len; i++) {
+			int current_element= last + before_last;
+			if (current_element%2==1) {numOfOdd+=1;};
+			before_last=last;
+			last=current_element;
+			series = series + " " + current_element;
+		}
+		int n = len;
 		
 		
 		System.out.println("The first "+ n +" Fibonacci numbers are:");
-		// *** your code goes here below ***
+		System.out.println(series);
+		
 		
 		System.out.println("The number of odd numbers is: "+numOfOdd);
 
