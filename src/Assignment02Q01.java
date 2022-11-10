f339832c05aeb22d0e470f66666f6210294133bd
DorGeyerGit <117093805+DorGeyerGit@users.noreply.github.com>
2022-11-10 17:12:13 +0200

    Update Assignment02Q01.java

--- src/Assignment02Q01.java
+++ src/Assignment02Q01.java
@@ -2,7 +2,11 @@
 public class Assignment02Q01 {
 
 	public static void main(String[] args) {
-		// *** your code goes here below ***
+		for (int i = 0; i < args.length; i++) {
+			int x = args[i].charAt(0);
+			if(x%5==0) {
+			  System.out.println(args[i].charAt(0));
+			}}
 		
 	}
 
